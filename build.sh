73116e3ccc05bb0060b75f6cdbea3f404c7081cf
suutaku <johnsuu@cotnetwork.com>
2022-06-09 11:18:34 +0800

    update build script

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
  platform=`uname`
 
+git submodule update --init --recursive
+
 if [ "$platform" = "Linux" ];then
   sudo apt install -y libx11-dev xorg-dev libxtst-dev
   sudo apt install -y xcb libxcb-xkb-dev x11-xkb-utils libx11-xcb-dev libxkbcommon-x11-dev libxkbcommon-dev
